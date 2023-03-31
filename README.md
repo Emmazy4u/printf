8ab332dce41b13f6c21d6851607e0d62d7b37641
emma-obelenge <obeleemmanuel@gmail.com>
2023-03-31 02:16:14 -0700

    more update from emma-obelenge on the printf project

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 printf teamwork
-.
+This team work on printf was done by Mr Watt Watt in collaboration with Emmanuel Obelenge.
--- _printf.c
+++ _printf.c
@@ -3,7 +3,7 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _printf - Printf function
+ * _printf - This is the Printf function
  * @format: format.
  * Return: Printed chars.
  */
